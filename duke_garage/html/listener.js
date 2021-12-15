57c621b31f99b0e924b607056911cffcc1c85ede
AirbladeHD <59345606+AirbladeHD@users.noreply.github.com>
2021-12-15 10:42:57 +0100

    Update

--- duke_garage/html/listener.js
+++ duke_garage/html/listener.js
@@ -1,3 +1,5 @@
+var id = "none"
+
 $(function(){
     $('#container').hide();
     window.onload = function(e) {
@@ -6,10 +8,25 @@ $(function(){
             if(item !== undefined && item.type === "ui") {
                 if(item.display === true) {
                     $('#container').show();
+                    vehicles = item.vehicles
+                    vehicles.forEach(function(e) {
+                        $('#vehicles').append("<div class='element' id='" + e['name'] + "'></div>")
+                    })
                 } else {
                     $('#container').hide();
                 }
             }
         })
+    $('.element').click(function(){
+        id = this.attr('id');
+    })
+    $('#out').click(function(){
+        $.post("http://duke_garage/out", JSON.stringify({
+            name: currentVehicle,
+            primary: $('#primary').val(),
+            secondary: $('#secondary').val(),
+            mods: mods_list
+        }));
+    })
     }
 })
